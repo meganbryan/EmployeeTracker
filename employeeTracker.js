a93c8d2b12f166cf1d4097149d4c6954e8fde958
Megan Bryan <meganmbryan12@gmail.com>
2021-01-31 11:22:35 -0800

    Establish connection to MySQL

---
+++ employeeTracker.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_DB",
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+});
