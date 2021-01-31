9f524bf68a649523820eecb055ca14b6dc1a2e40
Megan Bryan <meganmbryan12@gmail.com>
2021-01-31 11:01:49 -0800

    Fix typo of table 3

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -15,7 +15,7 @@ CREATE TABLE role(
     department_id INT NOT NULL
 );
 
-CREATE TABLE auctions(
+CREATE TABLE employee(
     id INT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
