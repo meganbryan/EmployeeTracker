6499a52f23f85ef80062982f88468a2623916a4c
Megan Bryan <meganmbryan12@gmail.com>
2021-01-31 11:27:43 -0800

    Add initial inquirer prompt

--- employeeTracker.js
+++ employeeTracker.js
@@ -9,6 +9,31 @@ const connection = mysql.createConnection({
     database: "employee_DB",
 });
 
+const init = () => {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "Would you like to add?",
+            choices: ["DEPARTMENT", "ROLE", "EMPLOYEE"],
+        })
+        .then((answer) => {
+            if (answer.action === "DEPARTMENT") {
+                console.log("ADD DEPARTMENT");
+            } 
+            else if (answer.action === "ROLE") {
+                console.log("ADD ROLE");
+            } 
+            else if (answer.action === "EMPLOYEE") {
+                console.log("ADD EMPLOYEE");
+            } 
+            else {
+                connection.end();
+            }
+        });
+};
+
 connection.connect((err) => {
     if (err) throw err;
+    init ()
 });
