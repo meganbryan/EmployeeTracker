2d640029316a2bd04259ea905c583e51b090df1d
Megan Bryan <meganmbryan12@gmail.com>
2021-01-31 22:50:07 -0800

    Add information to MySQL table to use as examples

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -22,3 +22,28 @@ CREATE TABLE employee(
     role_id INT NOT NULL,
     manager_id INT NULL
 );
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Megan", "Bryan", 12, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Jack", "Sherman", 24, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (3, "Lucy", "Lippincott", 36, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (12, "Manager", "70000", 10);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (24, "Engineer", "60000", 10);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (36, "Mechanic", "50000", 10);
+
+INSERT INTO department (id, name)
+VALUES (10, "Production");
+
+SELECT employee.id, first_name, last_name, role.title, role.salary, role.department_id, manager_id
+FROM employee
+RIGHT JOIN role ON employee.role_id = role.id;
